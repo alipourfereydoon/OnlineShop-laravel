eab510dbe07822b4b8270d3d1fc71035ba35b561
ali <ali.pourferydoon1983@gmail.com>
2024-05-31 17:46:39 +0330

    In this section of the site, the posts and pages have been expanded. In this section, you can create pages related to company introduction or store introduction and other pages. It should be noted that these pages that are not dynamic can be made in this section

--- project/resources/views/admin/content/page/create.blade.php
+++ project/resources/views/admin/content/page/create.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.master')
 
 @section('head-tag')
-<title>برند </title>
+<title> پیج ساز</title>
 @endsection
 
 @section('content')
@@ -11,8 +11,8 @@
       <li class="breadcrumb-item font-size-12"> <a href="#"> خانه </a></li>
 
       <li class="breadcrumb-item font-size-12"> <a href="#"> بخش فروش </a></li>
-     <li class="breadcrumb-item font-size-12"> <a href="#">برند </a></li>
-      <li class="breadcrumb-item active font-size-12" aria-current="page">ایجاد برند </li>
+     <li class="breadcrumb-item font-size-12"> <a href="#"> پیج ساز</a></li>
+      <li class="breadcrumb-item active font-size-12" aria-current="page">ایجاد پیج </li>
     </ol>
   </nav>
 
@@ -21,11 +21,11 @@
     <section class="col-12">
         <section class="main-body-container">
             <section class="main-body-container-header">
-                <h5> ایجاد  برند </h5>
+                <h5> ایجاد پیج  </h5>
             </section>
 
             <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
-                <a href="{{route('admin.market.brand.index')}}" class="btn btn-info btn-sm">بازگشت  </a>
+                <a href="{{route('admin.content.page.index')}}" class="btn btn-info btn-sm">بازگشت  </a>
             </section>
 
             <section >
@@ -33,17 +33,29 @@
                     <section class="row">
                         <section class="col-12 col-md-6">
                             <div class="form-group">
-                                <label for="">نام برند</label>
+                                <label for="">عنوان </label>
                                 <input type="text" class="form-control form-control-sm">
                             </div>
 
                         </section>
-                        <section  class="col-12 col-md-6">
+
+                        <section class="col-12 col-md-6">
                             <div class="form-group">
-                                <label for="">تصویر برند  </label>
-                               <input type="file" class="form-control form-control-sm">
+                                <label for=""> آدرس URL</label>
+                                <input type="text" class="form-control form-control-sm">
                             </div>
+
                         </section>
+
+
+                        <section class="col-12 ">
+                            <div class="form-group">
+                                <label for="">محتوا </label>
+                                <textarea name="body" id="body" class="form-control form-control-sm" rows="6"></textarea>                            </div>
+
+                        </section>
+
+
                         <section class="pt-4 col-12">
                             <button class="btn btn-primary btn-sm">ثبت</button>
                         </section>
@@ -62,3 +74,10 @@
 
 @endsection
 
+
+@section('script')
+<script src="{{ asset('admin-assets/ckeditor/ckeditor.js') }}"></script>
+<script>
+    CKEDITOR.replace('body');
+</script>
+@endsection
