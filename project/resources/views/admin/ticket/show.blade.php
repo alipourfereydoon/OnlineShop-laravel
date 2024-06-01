dd07d3ae4d88cdeeff4eb9c4af7fd6d7523f816a
ali <ali.pourferydoon1983@gmail.com>
2024-06-01 22:45:51 +0330

    If customers have problems while using the site, they can contact the site administrator and send their problems to the site administrators with tickets . in this section i designe the tcket section.

---
+++ project/resources/views/admin/ticket/show.blade.php
@@ -0,0 +1,76 @@
+@extends('admin.layouts.master')
+
+@section('head-tag')
+<title>نمایش تیکت ها </title>
+@endsection
+
+@section('content')
+
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item font-size-12"> <a href="#"> خانه </a></li>
+
+      <li class="breadcrumb-item font-size-12"> <a href="#"> بخش تیکت ها </a></li>
+     <li class="breadcrumb-item font-size-12"> <a href="#">تیکت ها  </a></li>
+      <li class="breadcrumb-item active font-size-12" aria-current="page">نمایش تیکت ها   </li>
+    </ol>
+  </nav>
+
+
+  <section class="row">
+    <section class="col-12">
+        <section class="main-body-container">
+            <section class="main-body-container-header">
+                <h5>نمایش تیکت ها </h5>
+            </section>
+
+            <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
+                <a href="{{route('admin.ticket.index')}}" class="btn btn-info btn-sm">بازگشت  </a>
+            </section>
+            <section class="card mb-3">
+                <section class="card-header text-white bg-custom-pink">
+                    علی پوفریدون -2298637611
+
+                </section>
+                <section class="card-body">
+                    <h5 class="card-title">
+                        مشخصات کالا: ساعت هوشمند
+                    </h5>
+                    <p class="card-text">
+                        تلفن خوبی نیست
+                    </p>
+
+                </section>
+
+            </section>
+
+            <section >
+                <form action="" method="">
+                    <section class="row">
+                        <section class="col-12 ">
+                            <div class="form-group">
+                                <label for="">پاسخ تیکت </label>
+                                <textarea class="form-control form-control-sm" >
+
+                                </textarea>
+                            </div>
+
+                        </section>
+                        <section class="pt-4 col-12">
+                            <button class="btn btn-primary btn-sm">ثبت</button>
+                        </section>
+
+                    </section>
+
+
+                </form>
+
+            </section>
+
+        </section>
+
+    </section>
+</section>
+
+@endsection
+
